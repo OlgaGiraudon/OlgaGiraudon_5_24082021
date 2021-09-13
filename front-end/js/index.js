8bcd4d02d7f0aef49ab6a03cf1aa3afce7d84a39
Olga Giraudon <giraudon.olga@gmail.com>
2021-09-13 18:18:28 +0200

    Ajout lien vers la page produits

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,22 +1,35 @@
 //initialization  of variables and constants//
 let  gridProduit = document.querySelector('#gridProduits');
-let imgHTML;
-
-
-/*const navIcon = document.querySelector('.navIcon');*/
 let url = 'http://localhost:3000/api/cameras';
-//Get the photos and show them on the main page
-fetch (url)
-.then((response)=>
+//function avec URL en parametres, resultat - code HTML//
+function getProducts(adresseAPI, gridOfProducts){
+    fetch (adresseAPI)
+    .then((response)=>
     response.json()
-)
-.then((data) => {
-for (let i in data){
-imgHTML = '<div class="imageProduit"> <h3 class="nameProduit">' + data[i].name + '</h3> <img  src="' + data[i].imageUrl + '" alt="" width = 100% ></div>';
-gridProduit.innerHTML += imgHTML;
+    )
+    .then((data) => {
+        if(data){
+        let imgHTML ='';
+        for (let i in data){
+            imgHTML += '<div class="imageProduit"> '+
+                '<h3 class="nameProduit">' + data[i].name + '</h3>'+
+                '<a href="produit.html?id='+data[i]._id+'">'+
+                 '<img  src="' + data[i].imageUrl + '" alt="Photo of '+ data[i].name +'" width ="100%"  ></a></div>';
+        }
+        gridOfProducts.innerHTML += imgHTML;
+    }
+
+    })
 }
-}
-);
+
+
+//Get the photos and show them on the main page
+getProducts(url, gridProduit);
+
+
+
+
+
 
 
 
