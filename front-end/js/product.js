58051244056d0ee0e3a3745e8823b35668ae83fd
Olga Giraudon <giraudon.olga@gmail.com>
2021-09-28 18:06:06 +0200

    Correction page Produit

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -3,9 +3,13 @@ let  productWindow = document.querySelector('#prodImage'); //div avec photo de p
 let productDescription = document.querySelector('#product_description'); //Paragraphe avec description du produit
 let productSize = document.querySelector('#lent'); //Taille de lentille
 let productPrise = document.querySelector('#product_price'); //Prix du produit
-let url = 'http://localhost:3000/api/cameras/5be1ed3f1c9d44000030b061';
+const params = new URLSearchParams(window.location.search);
+let url = 'http://localhost:3000/api/cameras/' +params.get('id');
+let quantityOfProduct = document.querySelector('#quantityOfProduct');
+let priceTotal = document.querySelector('#priceTotal');
 
-function showProduct(adresseAPI, productWindow, productDescription, productSize, productPrise) {
+
+function showProduct(adresseAPI, productWindow, productDescription, productSize, productPrise,) {
     fetch (adresseAPI)
     .then((response)=>
     response.json()
@@ -20,9 +24,14 @@ function showProduct(adresseAPI, productWindow, productDescription, productSize,
             let descriptionProd ='';
             descriptionProd = data.description;
             productDescription.innerHTML = descriptionProd;
+            
             let tailleObj = '';
-            tailleObj = data.lenses;
-            productSize.innerHTML = tailleObj;
+            
+            for(let elem in data.lenses){
+                tailleObj = '<option>'+ data.lenses[elem] +'</option>'
+                productSize.innerHTML += tailleObj;
+            }
+            
             let prixProd = '';
             prixProd = data.price;
             productPrise.innerHTML=prixProd/100 + '€';
@@ -32,3 +41,8 @@ function showProduct(adresseAPI, productWindow, productDescription, productSize,
 
 showProduct(url, productWindow, productDescription, productSize, productPrise);
 
+/*Montant total*/
+quantityOfProduct.addEventListener('change', function(){
+    
+    priceTotal.value = Number(productPrise.innerHTML.slice(0,-1))*quantityOfProduct.value + ' €';
+})
