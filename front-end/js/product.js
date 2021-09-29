cd9f3034edb262348b68dfff46f420202542875c
Olga Giraudon <giraudon.olga@gmail.com>
2021-09-29 10:48:50 +0200

    Correction partie CSS page Produit

--- front-end/js/product.js
+++ front-end/js/product.js
@@ -1,5 +1,5 @@
 let productName = document.querySelector('#product_name'); //h2 nom du produit
-let  productWindow = document.querySelector('#prodImage'); //div avec photo de produit
+let  productWindow = document.querySelector('#prodImageDiv'); //div avec photo de produit
 let productDescription = document.querySelector('#product_description'); //Paragraphe avec description du produit
 let productSize = document.querySelector('#lent'); //Taille de lentille
 let productPrise = document.querySelector('#product_price'); //Prix du produit
@@ -7,6 +7,7 @@ const params = new URLSearchParams(window.location.search);
 let url = 'http://localhost:3000/api/cameras/' +params.get('id');
 let quantityOfProduct = document.querySelector('#quantityOfProduct');
 let priceTotal = document.querySelector('#priceTotal');
+let button = document.querySelector('#buttonBuy');
 
 
 function showProduct(adresseAPI, productWindow, productDescription, productSize, productPrise,) {
@@ -16,14 +17,14 @@ function showProduct(adresseAPI, productWindow, productDescription, productSize,
     )
     .then((data) => {
             let product = '';
-            product =  '<img  src=" ' + data.imageUrl + '" width ="100%"></div>';
+            product =  '<img id="ProdImage" src=" ' + data.imageUrl + '" width ="100%"></div>';
             productWindow.innerHTML = product;
             let name = '';
             name = data.name;
             productName.innerHTML = name;
             let descriptionProd ='';
             descriptionProd = data.description;
-            productDescription.innerHTML = descriptionProd;
+            productDescription.innerHTML = "<b>Description du produit: </b><br> " + descriptionProd;
             
             let tailleObj = '';
             
@@ -34,15 +35,16 @@ function showProduct(adresseAPI, productWindow, productDescription, productSize,
             
             let prixProd = '';
             prixProd = data.price;
-            productPrise.innerHTML=prixProd/100 + '€';
-            
+            productPrise.innerHTML=prixProd/100 + ' €';
+            priceTotal.innerHTML ="Total à payer: " +'<b>'+productPrise.innerHTML + ' </b>';
     }
     )};
 
 showProduct(url, productWindow, productDescription, productSize, productPrise);
 
-/*Montant total*/
+/*Calcul montant total*/
+
 quantityOfProduct.addEventListener('change', function(){
     
-    priceTotal.value = Number(productPrise.innerHTML.slice(0,-1))*quantityOfProduct.value + ' €';
+    priceTotal.innerHTML = "Total à payer: " + '<b>' +Number(productPrise.innerHTML.slice(0,-1))*quantityOfProduct.value + ' € </b>';
 })
