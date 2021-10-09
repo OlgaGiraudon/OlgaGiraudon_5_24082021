e65ca571b107640c23b879d84002f5d2cd5b99da
Olga Giraudon <giraudon.olga@gmail.com>
2021-10-09 17:12:26 +0200

    Ajout suppression produit

--- front-end/js/panier.js
+++ front-end/js/panier.js
@@ -1,8 +1,38 @@
 let messageVide = document.querySelector('#messageVide');
 let mainDivPanier = document.querySelector('#mainDivPanier');
-let message = localStorage.getItem('message');
-if(message == null){
+let cartProducts = localStorage.getItem('cartProducts');
+let cameraNameTd = document.querySelector('#cameraNameTd');
+let cameraSizeTd = document.querySelector('#cameraSizeTd');
+let quantityTd = document.querySelector('#quantityTd');
+let priceUnit = document.querySelector('#priceUnit');
+let prodId = document.querySelector('#prodId');
+let suppression = document.querySelector('.fa-trash');
+
+if(cartProducts === null || JSON.parse(cartProducts).length == 0){
     
     mainDivPanier.innerHTML = '';
     messageVide.innerHTML = 'Votre panier est vide.';
+} else{
+    
+    messageVide.classList.add('small');
+    let product = JSON.parse(cartProducts);
+    cameraNameTd.innerHTML = product[0].name;
+    cameraSizeTd.innerHTML =  product[0].size;
+    quantityTd.innerHTML =  product[0].number;
+    priceUnit.innerHTML =  product[0].prixU + ' €';
+
+    prodId.innerHTML =  product[0].idProduit;
+
+    suppression.addEventListener('click', function() {
+        let idList = product.map(elem => elem.idProduit);
+        if(idList.includes(product[0].idProduit)){
+            let positionId = idList.indexOf(product[0].idProduit);
+            product.splice(positionId,1);
+            localStorage.setItem('cartProducts', JSON.stringify(product));
+            location.reload();
+        }
+        
+    })
+
+
 }
--- front-end/js/product.js
+++ front-end/js/product.js
@@ -4,11 +4,11 @@ let productDescription = document.querySelector('#product_description'); //Parag
 let productSize = document.querySelector('#lent'); //Taille de lentille
 let productPrise = document.querySelector('#product_price'); //Prix du produit
 const params = new URLSearchParams(window.location.search);
-let url = 'http://localhost:3000/api/cameras/' +params.get('id');
+let productId = params.get('id');
+let url = 'http://localhost:3000/api/cameras/' + productId;
 let quantityOfProduct = document.querySelector('#quantityOfProduct');
 let priceTotal = document.querySelector('#priceTotal');
 let button = document.querySelector('#buttonBuy');
-
 
 function showProduct(adresseAPI, productWindow, productDescription, productSize, productPrise,) {
     fetch (adresseAPI)
@@ -16,20 +16,22 @@ function showProduct(adresseAPI, productWindow, productDescription, productSize,
     response.json()
     )
     .then((data) => {
-       
+
             let product = '';
             product =  '<img id="ProdImage" src=" ' + data.imageUrl + '" width ="100%"></div>';
             productWindow.innerHTML = product;
+            
             let name = '';
             name = data.name;
             productName.innerHTML = name;
+            
             let descriptionProd ='';
             descriptionProd = data.description;
             productDescription.innerHTML = "<b>Description du produit: </b><br> " + descriptionProd;
             
             let tailleObj = '';
-            
             for(let elem in data.lenses){
+                sizeSelected = data.lenses[0];
                 tailleObj = '<option>'+ data.lenses[elem] +'</option>'
                 productSize.innerHTML += tailleObj;
             }
@@ -43,9 +45,32 @@ function showProduct(adresseAPI, productWindow, productDescription, productSize,
 
 showProduct(url, productWindow, productDescription, productSize, productPrise);
 
+/*Select taille*/
+let sizeSelected;
+productSize.addEventListener('change', function () {
+    sizeSelected=this.value;
+})
+/*Select quantité*/
+let numberSelected = 1;
+quantityOfProduct.addEventListener('change', function () {
+    numberSelected=this.value;
+})
 /*Calcul montant total*/
 
 quantityOfProduct.addEventListener('change', function(){
     
     priceTotal.innerHTML = "Total à payer: " + '<b>' +Number(productPrise.innerHTML.slice(0,-1))*quantityOfProduct.value + ' € </b>';
 })
+
+
+buttonBuy.addEventListener('click', function () {
+    
+    produitArray = [{'name': productName.innerHTML,
+                    'size': sizeSelected,
+                    'number': String(numberSelected),
+                    'prixU': String(productPrise.innerHTML.slice(0,-1)) ,
+                    'idProduit': productId}
+                ];
+    localStorage.setItem('cartProducts', JSON.stringify(produitArray));
+    
+});
