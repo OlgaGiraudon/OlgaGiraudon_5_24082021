668977443da28106e92b3d47fa7b9323e8f03b39
Olga Giraudon <giraudon.olga@gmail.com>
2021-10-12 18:36:18 +0200

    Validation formulaire

--- front-end/js/panier.js
+++ front-end/js/panier.js
@@ -16,8 +16,13 @@ let buttonPageDaccueil = document.querySelector('#buttonPageDaccueil');
 let buttonAcheter = document.querySelector("#buttonAcheter");
 let userSurname = document.querySelector('#userSurname');
 let userName = document.querySelector('#userName');
-
-
+let userAdress = document.querySelector('#userAdress');
+let userCity = document.querySelector('#userCity');
+let userPostalCode = document.querySelector('#userPostalCode');
+let userPhoneNumber = document.querySelector('#userPhoneNumber');
+let userMail = document.querySelector('#userMail');
+let champsObligatoiresArray = [userSurname,userName,userAdress, userCity, userPostalCode, userMail];
+let champsTousArray = [userSurname,userName,userAdress, userCity, userPostalCode, userPhoneNumber, userMail];
 if(cartProducts === null || JSON.parse(cartProducts).length == 0){
     
     mainDivPanier.innerHTML = '';
@@ -95,26 +100,81 @@ buttonViderpanier.addEventListener('click', function () {
 buttonPageDaccueil.addEventListener('click', function () {
     document.location.href = "index.html";
 });
-buttonAcheter.addEventListener('click', function(){
-   let regExprUserDataSurname =new RegExp(userSurname.value);
-   let regExprUserDataName = new RegExp(userName.value)
-        if(userSurname.value === ""){
-            alert('Veuillez saisir votre nom');
+/*Function verification si le champ de formulaire est vide*/
+function verifChampVide(champsArray) {
+    for(let elem of champsArray){
+        var varBoolean1 = true;
+        if(elem.value === ""){
+            alert('Veuillez saisir votre ' + elem.name);
+            varBoolean1 = false;
+            break;
+        } 
+    };
+    if (varBoolean1){
+        verifChampSaisie(champsTousArray);
+    };
+};
+/*Function verification la saisie du formulaire */
+function verifChampSaisie(champsArray){
+    let objDataUser ={};
+    var varBoolean = true;
+    for(let elem of champsArray){
+        let regExprUserData = elem.value;
+        if(elem.name == 'nom' || elem.name == 'prenom' || elem.name == 'ville'){
+            if(!/^[a-zA-Z-]+$/.test(regExprUserData)){
+                varBoolean = false;
+                alert('Le champ ' + elem.name + ' est invalide. Veuillez verifier la saisie');
+                break;
+                }
+                else if(elem.name == 'nom'){
+                        objDataUser.surname = elem.value;
+                        
+                    }
+                    else if(elem.name == 'prenom'){
+                        objDataUser.name = elem.value;
+                        
+                    }
+                    else if(elem.name == 'ville'){
+                        objDataUser.city = elem.value;
+                    };
         }
-            else if(userName.value === ""){
-                alert('Veuillez saisir votre nom et prénom ');
-            } 
-            else if(/\d/.test(regExprUserDataSurname)){
-            alert('Vérifier lorthographe de votre nom de famille');
+        else if(elem.name == 'adresse'){
+            objDataUser.adress = elem.value;
+        }
+        else if(elem.name == 'code_postal'){
+            if(!/^[a-zA-Z0-9]+$/.test(regExprUserData)){
+                alert('Le champ "Code postal" est invalide. Veuillez verifier la saisie');
+                varBoolean = false;
+                break;
             }
-            else if(/\d/.test(regExprUserDataName)){
-                alert('Vérifier lorthographe de votre prénom');
+            else {
+                objDataUser.code = elem.value;
             }
-        else {
-                localStorage.setItem('surname', JSON.stringify(userSurname.value));
-                localStorage.setItem('name', JSON.stringify(userName.value));
-                document.location.href = "confirmation.html";
-            }
+        }
+        else if(elem.name == 'numero'){
+            if(!/^[0-9]+$/.test(regExprUserData)){
+                alert('Le champ "Numéro de téléphone" est invalide. Veuillez verifier la saisie');
+                varBoolean = false;
+                break;
+            };
+        }
+        else if(elem.name == 'email'){
+            if(!/^.+\@+.+\..+$/.test(regExprUserData)){
+                alert('Le champ "Email" est invalide. Veuillez verifier la saisie');
+                varBoolean = false;
+                break;
+            };
+        };
+    };
+    localStorage.setItem('userData', JSON.stringify(objDataUser));
+    if(varBoolean){
+        document.location.href = "confirmation.html";
+    }
    
+};
+
+buttonAcheter.addEventListener('click', function(){
+    verifChampVide(champsObligatoiresArray);
+    
 });
 
